8c124a95c6943f0aa09e9c5574a6dc0a27aef3d0
Gouzi <ayagouzi@gmail.com>
2023-03-21 17:37:30 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *main - void
+ *
+ *Return: 0
+ *
+ **/
+
+int main(void)
+{
+	int a = 0;
+	char str[] = "_putchar";
+
+	while (str[a] >= a)
+	{
+	_putchar(str[a]);
+	a++;
+	}
+	_putchar('\n');
+	return (0);
+}
